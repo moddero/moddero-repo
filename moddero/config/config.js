3eab1fdea8bfe4d658f1a5c5a290ae53eaafa851
Zhelyo Papazov <zheliostanislavov@gmail.com>
2016-12-15 02:40:48 +0200

    A new beginning of the project.

--- moddero/config/config.js
+++ moddero/config/config.js
@@ -3,10 +3,7 @@ const path = require('path');
 module.exports = {
     development: {
         rootFolder: path.normalize(path.join(__dirname, '/../')),
-        connectionString: 'mongodb://localhost:27017/blog'
+        connectionString: 'mongodb://localhost:27017/moddero'
     },
     production:{}
 };
-
-
-
